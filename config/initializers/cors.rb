4d151d5c6a8f5bbd3857db479412121423d860e2
Mark Momot <markmomot03@gmail.com>
2022-04-30 22:25:02 -0400

    Attempt to make api only accessable from front end

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -9,7 +9,6 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins "http://www.markmomot.com"
 
-
-    resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete]
+    resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete], credentials: true
   end
 end
