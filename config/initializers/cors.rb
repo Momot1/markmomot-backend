0c3a59c7fb13af02252440e9f9058188be0e0f95
Mark Momot <markmomot03@gmail.com>
2022-05-06 22:53:24 -0400

    Attempt to fix signup

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "https://www.markmomot.com "
+    origins "*"
 
     resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete]
   end
