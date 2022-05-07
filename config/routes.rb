5f27f356ad87aa8372f124e8cbf93a3e098627cf
Mark Momot <markmomot03@gmail.com>
2022-05-06 22:15:21 -0400

    Added user and sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
-  resources :birds
+  
+    # Login/logout routes
+    post "/login", to: "sessions#create"
+    delete "/logout", to: "sessions#destroy"
+
+    # Signup and crab user info routes
+    post "/signup", to: "users#create"
+    get "/me", to: "users#show"
+
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,6 +1,6 @@
 require "test_helper"
 
-class BirdTest < ActiveSupport::TestCase
+class UsersControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
